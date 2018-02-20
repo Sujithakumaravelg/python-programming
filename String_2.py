9c71cd3e0df93948da03a4d7ca112e33ae13e729
Sujithakumaravelg <36624092+Sujithakumaravelg@users.noreply.github.com>
2018-02-20 12:06:35 +0530

    Create String_2.py

---
+++ String_2.py
@@ -0,0 +1,12 @@
+def main():
+    str_1='programmer'
+    a1=str_1.count('r',0,10)
+    print a1
+    a2=str_1.find('g')
+    print a2
+    a3=str_1.replace('m','y')
+    print a3
+
+
+if __name__ == '__main__':
+    main()
