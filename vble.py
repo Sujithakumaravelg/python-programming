043ef36684a2c7a3e91d9b9791bb4c73735ed9c8
Sujithakumaravelg <36624092+Sujithakumaravelg@users.noreply.github.com>
2018-02-20 11:57:24 +0530

    Create vble.py

---
+++ vble.py
@@ -0,0 +1,22 @@
+def main():
+    a1=5
+    a2=10
+    a3=54.89
+    a4=348.0
+    a5=0
+    a6=9234.9
+    a7=78.8734888887
+    a8=78
+    a9=a1*a6
+    a10=a2%a6
+    print(a1+a8)
+    print(a5-a6)
+    print(a9*a4)
+    print(a8**a1)
+    print(a6//a2)
+    print(a3/a8)
+
+
+
+if __name__ == '__main__':
+    main()
