aa744b6ec57161ec21e55f7b0c532cf0baea2f9a
Sujithakumaravelg <36624092+Sujithakumaravelg@users.noreply.github.com>
2018-05-29 19:57:28 -0700

    Update camelcase1.py

--- camelcase1.py
+++ camelcase1.py
@@ -1,12 +1,15 @@
 def camelcase():
-    s1=raw_input()
-    a=s1.split(' ')
-    g=" "
-    for i in range(0,len(a)):
+    s=raw_input()
+    if(len(s)<=1000000):
+        a=s.split(' ')
+        g=" "
+        for i in range(0,len(a)):
                     a[i]=a[i].capitalize()
-    c=a[0]
-    for i in range(1,len(a)):
+        c=a[0]
+        for i in range(1,len(a)):
                     c=c+g+a[i]
-    print c
+        print c
+    else:
+        print("Enter Valid Value")
 camelcase()
 
